22c0d6384ea209a53f3d4076d6f1ea8958645cb1
AdamRakaska <adamwhitehat@outlook.com>
2018-06-04 18:59:03 -0600

    Dead code cleanup

--- EntityJustworksGUI/MainForm.cs
+++ EntityJustworksGUI/MainForm.cs
@@ -1,221 +1,193 @@
 ﻿using System;
 using System.IO;
-using System.Text;
-using System.Drawing;
-using System.Configuration;
-using System.Windows.Forms;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
-//
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Linq.Expressions;
-//
 using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
-using System.Xml.Serialization;
-//
 using System.Linq;
 using System.Data;
-using System.Data.Sql;
-using System.Data.Linq;
-using System.Data.Common;
 using System.Data.SqlClient;
-using System.Data.Linq.SqlClient;
-//
-using Microsoft.SqlServer;
-using Microsoft.SqlServer.Server;
-using System.Data.SqlServerCe;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 using EntityJustWorks.SQL;
 
-
 namespace EntityJustworksGUI
 {
-    public partial class MainForm : Form
-    {
-        public static string ConnectionString_Format = "Server={0};Database={1};User ID={2};Password={3};";
+	public partial class MainForm : Form
+	{
+		public static string ConnectionString_Format = "Server={0};Database={1};User ID={2};Password={3};";
 		public static string ConnectionString_CsvFile = "Provider=Microsoft.Jet.OleDb.4.0;Data Source=C:\\TEMP\\database_orders.csv;Extended Properties=\"Text;HDR=YES;FMT=Delimited\"";
-        public static string ConnectionString_LocalDB = ConfigurationManager.AppSettings["ConnectionString_LocalDB"].ToString();
-        
-        public bool IsConnected
-        {
-            get { return _isConnected; }
-            set
-            {
-                _isConnected = value;
+		public static string ConnectionString_LocalDB = ConfigurationManager.AppSettings["ConnectionString_LocalDB"].ToString();
+
+		public bool IsConnected
+		{
+			get { return _isConnected; }
+			set
+			{
+				_isConnected = value;
 				//OnConnectionChanged();
 				groupBoxCommands.Enabled = _isConnected;
 				btnSQLConnect.Text = ConnectButtonText;
 			}
-        }
-        private bool _isConnected;
+		}
+		private bool _isConnected;
 
-        public string ConnectButtonText
-        {
-            get { return IsConnected ? "Disconnect..." : "Connect..."; }
-            //set { btnSQLConnect.Text = value; }
-        }
+		public string ConnectButtonText
+		{
+			get { return IsConnected ? "Disconnect..." : "Connect..."; }
+			//set { btnSQLConnect.Text = value; }
+		}
 
-        public string ConnectionStringText
-        {
-            get { return tbConnectionString.Text; }
-            set { tbConnectionString.Text = value; }
-        }
+		public string ConnectionStringText
+		{
+			get { return tbConnectionString.Text; }
+			set { tbConnectionString.Text = value; }
+		}
 
-        public string SelectCommand
-        {
-            get { return tbSelectCommand.Text; }
-            set { tbSelectCommand.Text = value; }
-        }
+		public string SelectCommand
+		{
+			get { return tbSelectCommand.Text; }
+			set { tbSelectCommand.Text = value; }
+		}
 
-        public string TableName
-        {
-            get { return tbTableName.Text; }
-            set { tbTableName.Text = value; }
-        }
+		public string TableName
+		{
+			get { return tbTableName.Text; }
+			set { tbTableName.Text = value; }
+		}
 
-        public SqlConnectionStringBuilder ConnectionString { get; set; }
+		public DataTable Datatable
+		{
+			get { return _table; }
+			set
+			{
+				_table = value;
+			}
+		}
+		private DataTable _table;
 
-        public DataTable Datatable
-        {
-            get { return _table; }
-            set
-            {
-                _table = value;
-                //IsConnected = Helper.IsValidDatatable(value, true);
-            }
-        }
-        private DataTable _table;
+		public FileInfo OutputFile
+		{
+			get { return new FileInfo(tbOutputFilename.Text); }
+			set { tbOutputFilename.Text = value.FullName; }
+		}
 
-        public FileInfo OutputFile
-        {
-            get { return new FileInfo(tbOutputFilename.Text); }
-            set { tbOutputFilename.Text = value.FullName; }
-        }
+		public SqlConnectionStringBuilder ConnectionString { get; set; }
 
-        public MainForm()
-        {
-            InitializeComponent();
-			
-            ConnectionStringText = ConnectionString_LocalDB;
-            SelectCommand = "SELECT TOP 1 * FROM [{0}] WHERE 1=1";
-            TableName = "Customers";
-            OutputFile = new FileInfo("Output.txt");
+		public MainForm()
+		{
+			InitializeComponent();
+
+			ConnectionStringText = ConnectionString_LocalDB;
+			SelectCommand = "SELECT TOP 1 * FROM [{0}] WHERE 1=1";
+			TableName = "Customers";
+			OutputFile = new FileInfo("Output.txt");
 
 			_table = new DataTable();
 			IsConnected = false;
 		}
 
-		
-
-
-
 		#region Button Events
 
 		private void btnSQLConnect_Click(object sender, EventArgs e)
-        {
-            if (!IsConnected)
-            {
-                ConnectionString = new SqlConnectionStringBuilder(ConnectionStringText);
+		{
+			if (!IsConnected)
+			{
+				ConnectionString = new SqlConnectionStringBuilder(ConnectionStringText);
 
-                Datatable = Table.FromQuery(ConnectionString.ConnectionString, SelectCommand, TableName);
-                Datatable.TableName = TableName;
-                IsConnected = Helper.IsValidDatatable(Datatable, true);
+				Datatable = Table.FromQuery(ConnectionString.ConnectionString, SelectCommand, TableName);
+				Datatable.TableName = TableName;
+				IsConnected = Helper.IsValidDatatable(Datatable, true);
 
-                btnScript_InsertInto.Enabled = (Datatable.Rows.Count > 0);
+				btnScript_InsertInto.Enabled = (Datatable.Rows.Count > 0);
 
-                IEnumerable<string> collumnNames = Datatable.Columns.OfType<DataColumn>().Select(col => col.ColumnName);
-                tbOutput.Text = Helper.ListToDelimitedString(collumnNames, "\t @", ",\n\t@", "\n");
-            }
-            else
-            {
-                //if (ConnectionString != null) {
-                ConnectionString.Clear();
-                ConnectionString = new SqlConnectionStringBuilder();
-                //} if (Datatable != null) {
-                Datatable.Clear();
-                Datatable = new DataTable();
+				IEnumerable<string> collumnNames = Datatable.Columns.OfType<DataColumn>().Select(col => col.ColumnName);
+				tbOutput.Text = Helper.ListToDelimitedString(collumnNames, "\t @", ",\n\t@", "\n");
+			}
+			else
+			{
+				//if (ConnectionString != null) {
+				ConnectionString.Clear();
+				ConnectionString = new SqlConnectionStringBuilder();
+				//} if (Datatable != null) {
+				Datatable.Clear();
+				Datatable = new DataTable();
 				Datatable = TestObjectsFactory.BuildDatatable();
-                //}
+				//}
 
-                IsConnected = false;
-            }           
-        }
+				IsConnected = false;
+			}
+		}
 
-        private void btnBuild_CSharpCode_Click(object sender, EventArgs e)
-        {
-            OutputFile = Table.ToCSharpCode(Datatable, "DataTable.cs");
-        }
+		private void btnBuild_CSharpCode_Click(object sender, EventArgs e)
+		{
+			OutputFile = Table.ToCSharpCode(Datatable, "DataTable.cs");
+		}
 
-        private void btnBuild_Assembly_Click(object sender, EventArgs e)
-        {
-            Type dataclassAssembly = Table.ToAssembly(Datatable);
-        }
+		private void btnBuild_Assembly_Click(object sender, EventArgs e)
+		{
+			Type dataclassAssembly = Table.ToAssembly(Datatable);
+		}
 
-        private void btnBuild_XML_Click(object sender, EventArgs e)
-        {
-            XmlSerializerFactory xmlFactory = new XmlSerializerFactory();
-            XmlSerializer datarowSerializer = xmlFactory.CreateSerializer(typeof(List<DataRow>));
+		private void btnBuild_XML_Click(object sender, EventArgs e)
+		{
+			XmlSerializerFactory xmlFactory = new XmlSerializerFactory();
+			XmlSerializer datarowSerializer = xmlFactory.CreateSerializer(typeof(List<DataRow>));
 
-            OutputFile = new FileInfo("DataTable.xml");
+			OutputFile = new FileInfo("DataTable.xml");
 
-            XmlWriter xWriter = XmlWriter.Create(OutputFile.FullName);
-            datarowSerializer.Serialize(xWriter, Datatable.Rows.Cast<DataRow>().ToList());
-        }
+			XmlWriter xWriter = XmlWriter.Create(OutputFile.FullName);
+			datarowSerializer.Serialize(xWriter, Datatable.Rows.Cast<DataRow>().ToList());
+		}
 
-        private void btnBuild_JSON_Click(object sender, EventArgs e)
-        {
-            OutputFile = new FileInfo("DataTable.json");
-            Stream jsonStream = new FileStream(OutputFile.FullName, FileMode.CreateNew);
-            XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(jsonStream);
-            //jsonWriter.WriteArray("","DataRow","",
-        }
+		private void btnBuild_JSON_Click(object sender, EventArgs e)
+		{
+			OutputFile = new FileInfo("DataTable.json");
+			Stream jsonStream = new FileStream(OutputFile.FullName, FileMode.CreateNew);
+			XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(jsonStream);
+		}
 
-        private void btnScript_CreateTable_Click(object sender, EventArgs e)
-        {
-            string scriptText = SQLScript.CreateTable(Datatable);
-            tbOutput.Text = scriptText;
+		private void btnScript_CreateTable_Click(object sender, EventArgs e)
+		{
+			string scriptText = SQLScript.CreateTable(Datatable);
+			tbOutput.Text = scriptText;
 
-            OutputFile = new FileInfo(string.Format("{0}.CreateTable.sql", Datatable.TableName));
-            File.WriteAllText(OutputFile.FullName, scriptText);
-        }
+			OutputFile = new FileInfo(string.Format("{0}.CreateTable.sql", Datatable.TableName));
+			File.WriteAllText(OutputFile.FullName, scriptText);
+		}
 
-        private void btnScript_InsertInto_Click(object sender, EventArgs e)
-        {
-            string scriptText = SQLScript.InsertInto(Datatable);
-            tbOutput.Text = scriptText;
+		private void btnScript_InsertInto_Click(object sender, EventArgs e)
+		{
+			string scriptText = SQLScript.InsertInto(Datatable);
+			tbOutput.Text = scriptText;
 
-            OutputFile = new FileInfo(string.Format("{0}.InsertInto.sql", Datatable.TableName));
-            File.WriteAllText(OutputFile.FullName, scriptText);
+			OutputFile = new FileInfo(string.Format("{0}.InsertInto.sql", Datatable.TableName));
+			File.WriteAllText(OutputFile.FullName, scriptText);
 
-        }
+		}
 
-        private void btnScript_StoredProcedure_Click(object sender, EventArgs e)
-        {
-            string scriptText = SQLScript.StoredProcedure.Update(Datatable,"{WhereClause}");
-            scriptText = scriptText.Replace("{DatabaseName}", ConnectionString.InitialCatalog);
-            scriptText = scriptText.Replace("{StoredProcedureName}", string.Format("sp_{0}_InsertInto", Datatable.TableName.Replace("_","")));
+		private void btnScript_StoredProcedure_Click(object sender, EventArgs e)
+		{
+			string scriptText = SQLScript.StoredProcedure.Update(Datatable, "{WhereClause}");
+			scriptText = scriptText.Replace("{DatabaseName}", ConnectionString.InitialCatalog);
+			scriptText = scriptText.Replace("{StoredProcedureName}", string.Format("sp_{0}_InsertInto", Datatable.TableName.Replace("_", "")));
 
-            tbOutput.Text = scriptText;
+			tbOutput.Text = scriptText;
 
-            OutputFile = new FileInfo(string.Format("{0}.StoredProcedure.sql", Datatable.TableName));
-            File.WriteAllText(OutputFile.FullName, scriptText);
-        }
+			OutputFile = new FileInfo(string.Format("{0}.StoredProcedure.sql", Datatable.TableName));
+			File.WriteAllText(OutputFile.FullName, scriptText);
+		}
 
-        private void btnScript_DataBase_Click(object sender, EventArgs e)
-        {
+		private void btnScript_DataBase_Click(object sender, EventArgs e)
+		{
 			//string scriptText = SQLScript.DataBase(ConnectionStringText);
 			//tbOutputText.Text = scriptText;
 			//
 			//OutputFile = new FileInfo(string.Format("DataBase.sql", Datatable.TableName));
 			//File.WriteAllText(OutputFile.FullName, scriptText);
-        }
-		
+		}
+
 		private void btnScript_InsertIntoParameterized_Click(object sender, EventArgs e)
 		{
 			DataTable table = TestObjectsFactory.BuildDatatable();
@@ -223,19 +195,8 @@ namespace EntityJustworksGUI
 			string insertIntoScript = SQLScript.InsertInto(table);
 
 			tbOutput.Text = insertIntoScript;
-
-			//SqlConnectionStringBuilder connstrBuilder = new SqlConnectionStringBuilder();
-			//connstrBuilder.AttachDBFilename = "Database001.mdf";
-			//connstrBuilder.InitialCatalog = "master";//"MyData";
-			
-			//SqlCeConnection connection = new SqlCeConnection(/*@"Data Source=C:\Users\CodeNinja\Documents\Visual Studio 2015\Projects\EntityJustWorks\EntityJustworksGUI\Database001.mdf;"*/connstrBuilder.ConnectionString);
-			//connection.Open();
-
-			//SqlCeCommand command = new SqlCeCommand("CREATE DATABASE MyData", connection);
-			
-			//List<DataRow> failedRows = Table.ToSqlTable("whatever", table);
 		}
-		
+
 		private void btnGenerateSampleData_Click(object sender, EventArgs e)
 		{
 			Datatable = TestObjectsFactory.BuildDatatable(6);
